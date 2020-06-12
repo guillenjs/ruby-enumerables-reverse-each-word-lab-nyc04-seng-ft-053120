5973f6bf6d38a94d03049ca6efc4a3fd0732114a
Jorge Guillen <jorgeguilln@gmail.com>
2020-06-12 21:28:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,8 +6,11 @@ def reverse_each_word(string)
   #   new << word.reverse
   # end
   # new.join(' ')
-
 string.split.collect {|word| word.reverse }.join(' ')
-
-
 end
+
+# Without iterating:
+
+# def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+# end
